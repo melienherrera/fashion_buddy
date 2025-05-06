c08d69b10f2b4fab75eb458b05c271f736663134
melienherrera <85907755+melienherrera@users.noreply.github.com>
2025-05-06 14:00:35 -0700

    Update proxy.js
    Passing through CORS headers

--- proxy.js
+++ proxy.js
@@ -2,6 +2,13 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 
 // for Vercel deployment
 export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
   return createProxyMiddleware({
     target: 'https://langflow-manual-install.onrender.com',
     changeOrigin: true,
